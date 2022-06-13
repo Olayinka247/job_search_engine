d527c899434839d3fe32aa82029702f44e1e6114
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-06-13 11:40:58 +0100

    Search

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Search</Form.Label>
+              <Form.Control type="text" placeholder="Search" />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
